92354cfaf6170748c63e3371e7bcfd8f837a2683
Latemarch <dolphin0331@naver.com>
2023-08-29 13:06:35 +0900

    feat: add and apply fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div>world</div>;
+  return (
+    <div className="p-10 text-xl font-bold">
+      <h1 className="">Roboto_Mono font test</h1>
+      <p className="font-sans">Roboto_Mono font test</p>
+      <p>한글폰트테스트</p>
+    </div>
+  );
 }
