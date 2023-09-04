50b7580e0c895c43d4eca44f06be06926271f2ca
LateMarch <73418661+Latemarch@users.noreply.github.com>
2023-09-04 14:37:28 +0900

    add dockerfile

--- src/components/InputName.tsx
+++ src/components/InputName.tsx
@@ -1,10 +1,10 @@
-'use client'
-import useSWR from 'swr'
+"use client";
+import useSWR from "swr";
 
 const fetcher = async (url: string) =>
-  await fetch(url).then((res) => res.json())
+  await fetch(url).then((res) => res.json());
 
 export default function InputName() {
-  const { data } = useSWR('/api/gethi', fetcher)
-  return data && <div>{data.msg}</div>
+  const { data } = useSWR("/api/gethi", fetcher);
+  return data && <div>{data.msg}</div>;
 }
